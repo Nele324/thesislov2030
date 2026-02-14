a0aa844edd3e11dbe90c06ae8b988956e354a6f0
lotte <lotte.saye@student.kuleuven.be>
2026-02-14 17:11:31 +0100

    test 5

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello test 4<code>src/App.tsx</code> world!.
+          Hello<code>src/App.tsx</code> world!.
         </p>
         <a
           className="App-link"
