ecd69363cda4f9c876d4f05fdeefbc6c109ef994
nele <nele.lauryssen@student.kuleuven;be>
2026-02-18 12:09:04 +0100

    Nele test CICD

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -15,7 +15,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>src/App.tsx</code> world!.
+          Hello 2 <code>src/App.tsx</code> world!.
         </p>
         <a
           className="App-link"
